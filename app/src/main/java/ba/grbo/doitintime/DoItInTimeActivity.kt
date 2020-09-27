3206931a1a5ca97ffdbc63383181337b0b2781c6
Jasmin Grbo <grbo.dev@gmail.com>
2020-09-27 07:22:35 +0200

    Initial commit

---
+++ app/src/main/java/ba/grbo/doitintime/DoItInTimeActivity.kt
@@ -0,0 +1,11 @@
+package ba.grbo.doitintime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoItInTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
